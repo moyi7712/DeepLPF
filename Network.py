ca73dab5f6d7cd6bb85dc066d15075c1fbb48bbf
haiya <mori7712@hnu.edu.cn>
2020-09-10 20:25:49 +0800

    fix some bug

--- Network.py
+++ Network.py
@@ -2,6 +2,8 @@ from Models import DeepLPF
 import tensorflow as tf
 import tensorflow.keras as keras
 import os
+import Utils
+
 
 class Net(object):
     def __init__(self, config):
@@ -17,57 +19,26 @@ class Net(object):
         if self.ckpt_manager.latest_checkpoint:
             checkpoint.restore(self.ckpt_manager.latest_checkpoint)
 
-<<<<<<< HEAD
-    @staticmethod
-    def rgb2Lab(image):
-=======
-    # @staticmethod
-    def rgb2Lab(self, image):
->>>>>>> 07fa757d3e4ef5bce28a0cc2cb361290bef14222
-        image = tf.where(image > 0.04045, ((image + 0.055) / 1.055) ** 2.4, image / 12.92)
-        X = image[:, :, :, 0] * 0.412453 + image[:, :, :, 1] * 0.357580 + image[:, :, :, 2] * 0.180423
-        Y = image[:, :, :, 0] * 0.212671 + image[:, :, :, 1] * 0.715160 + image[:, :, :, 2] * 0.072169
-        Z = image[:, :, :, 0] * 0.019334 + image[:, :, :, 1] * 0.119193 + image[:, :, :, 2] * 0.950227
-        eps = 6 / 29
-        Xn = (X / 0.950456)
-        Yn = (Y / 1.0)
-        Zn = (Z / 1.088754)
-        f = lambda value: tf.where(value > eps ** 3, value ** (1 / 3), ((1 / 3) * (1 / eps) ** 2) * value + 4 / 29)
-        L = tf.expand_dims(116 * f(Yn) - 16, axis=3)
-        a = tf.expand_dims(500 * (f(Xn) - f(Yn)), axis=3)
-        b = tf.expand_dims(200 * (f(Yn) - f(Zn)), axis=3)
-        L = L / 100
-        a = ((a / 110) - 1) / 2
-        b = ((b / 110) - 1) / 2
-        Lab = tf.concat(values=[L, a, b], axis=3)
-        return Lab
-
     def loss(self, predict, label):
-        predict_Lab = self.rgb2Lab(predict)
-        label_Lab = self.rgb2Lab(label)
+        predict_Lab = Utils.rgb2Lab(predict)
+        label_Lab = Utils.rgb2Lab(label)
 
         L_p, _, _ = tf.split(predict_Lab, 3, axis=3)
         L_l, _, _ = tf.split(label_Lab, 3, axis=3)
         loss_L = tf.reduce_mean(tf.abs(predict_Lab - label_Lab))
-<<<<<<< HEAD
+
         loss_ms_ssim = tf.image.ssim_multiscale(L_p, L_l, max_val=1.0,
                                                 filter_size=5, filter_sigma=0.5)
-        loss_ms_ssim = tf.reduce_mean(1-loss_ms_ssim)
-        loss = tf.reduce_mean(self.lamda_l1*loss_L + self.lamda_ms*loss_ms_ssim)
-=======
-        loss_ms_ssim = tf.image.ssim_multiscale(L_p, L_l, 1.0,
-                                                filter_size=5, filter_sigma=0.5)
-        loss_ms_ssim = tf.reduce_mean(1-loss_ms_ssim)
-        loss = self.lamda_l1*loss_L + self.lamda_ms*loss_ms_ssim
->>>>>>> 07fa757d3e4ef5bce28a0cc2cb361290bef14222
+        loss_ms_ssim = tf.reduce_mean(1 - loss_ms_ssim)
+        loss = tf.reduce_mean(self.lamda_l1 * loss_L + self.lamda_ms * loss_ms_ssim)
         return loss, loss_L, loss_ms_ssim
 
     def train(self, inputs, labels):
         with tf.GradientTape() as tape:
             predict = self.model(inputs)
+
             loss, loss_L, loss_ssim = self.loss(predict, labels)
 
         gradients = tape.gradient(loss, self.model.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
         return loss, loss_L, loss_ssim
-
